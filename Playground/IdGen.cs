8a0587c38cb7f8f5b201cdaa6b2f35c9bd702470
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2022-11-01 14:12:29 +0100

    cleanup master

--- Playground/IdGen.cs
+++
@@ -1,5 +0,0 @@
-public static class IdGen
-{
-    private static readonly Random Random = new();
-    public static long RandomLong() => Random.NextInt64();
-}
--- Playground/InMemoryDocumentStorage.cs
+++
@@ -1,6 +0,0 @@
-namespace Playground;
-
-internal class InMemoryDocumentStorage
-{
-    public Dictionary<long, Product> Products { get; set; } = new();
-}
